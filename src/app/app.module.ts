eb652f9624bf8ae7fd7d81c7768dfb7fb0e75ef9
kevin <kevin@bernardo.com>
2022-04-06 17:38:51 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar.component';
+import { ListComponent } from './components/users/list/list.component';
+import { DetailsComponent } from './components/users/details/details.component';
+import { RouterModule, Routes } from '@angular/router';
+import { Breadcrumb } from 'angular-crumbs';
+
+const appRoutes: Routes =[
+  { path: '', component: SidebarComponent },
+  { path: 'users', component: ListComponent, data: {breadcrumb: 'Users'} },
+  { path: 'user', component: DetailsComponent, data: {breadcrumb: 'User'} }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    Breadcrumb
   ],
   providers: [],
   bootstrap: [AppComponent]
